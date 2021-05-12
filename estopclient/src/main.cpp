ed7fb0bbe844a9fcbb0a1bf0e4152a12a6301927
Pete <github@kthxbye.us>
2021-05-12 22:40:42 +0800

    ADD: first draft remote estop

--- estopclient/src/main.cpp
+++ estopclient/src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#define BUTTON_PIN_D2 4
+#define LOOP_DELAY 20
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN_D2, INPUT);
+  delay(10);
+  Serial.flush();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool eStopFree = digitalRead(BUTTON_PIN_D2) > 0; 
+  bool previousEStopFree = !eStopFree; 
+
+  while(true){
+    eStopFree = digitalRead(BUTTON_PIN_D2) > 0;
+    digitalWrite(LED_BUILTIN, eStopFree);
+
+    if(eStopFree != previousEStopFree){
+      Serial.printf("Estop free changed %d -> %d\n", previousEStopFree, eStopFree);
+    }
+    previousEStopFree = eStopFree;
+    Serial.flush();
+    delay(LOOP_DELAY);
+  }  
 }
