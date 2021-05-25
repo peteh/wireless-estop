fd1324e8f8953fceee5597a2a916b6336ba607af
Pete <github@kthxbye.us>
2021-05-25 21:49:12 +0800

    ADD: virtual destructor to logger interface

--- libestop/lib/logger/src/Logger.h
+++ libestop/lib/logger/src/Logger.h
@@ -7,11 +7,12 @@ public:
     virtual void debug(const char *str) = 0;
 
     virtual void info(const char *str) = 0;
-    
-    virtual void warn(const char *str) = 0;
- 
-    virtual void error(const char *str) = 0;
-};
 
+    virtual void warn(const char *str) = 0;
+
+    virtual void error(const char *str) = 0;
+
+    virtual ~ILogger(){};
+};
 
 #endif
