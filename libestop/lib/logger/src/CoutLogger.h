e26cd22ffeeb789af1971cc5a25ebb8b71e54df3
Pete <github@kthxbye.us>
2021-08-06 11:37:37 +0800

    CHG: update logger

--- libestop/lib/logger/src/CoutLogger.h
+++ libestop/lib/logger/src/CoutLogger.h
@@ -1,28 +1,28 @@
 #ifndef COUT_LOGGER_H
 #define COUT_LOGGER_H
-#include <iostream>
+//#include <iostream>
 #include "Logger.h"
 
 class CoutLogger : public ILogger
 {
     void debug(const char *str) override
     {
-        std::cout << "[DEBUG] "<< str << std::endl;
+        //std::cout << "[DEBUG] "<< str << std::endl;
     }
 
     void info(const char *str) override
     {
-        std::cout << "[INFO] "<< str << std::endl;
+        //std::cout << "[INFO] "<< str << std::endl;
     }
 
     void warn(const char *str) override
     {
-        std::cout << "[WARN] " << str << std::endl;
+        //std::cout << "[WARN] " << str << std::endl;
     }
 
     void error(const char *str) override
     {
-        std::cerr << "[ERROR] " << str << std::endl;
+        //std::cerr << "[ERROR] " << str << std::endl;
     }
     
 };
